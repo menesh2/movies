74bdf16e6bed730bbd0e6fa901a36beec34e85a4
Or Menashe <menesh2@gmail.com>
2021-03-31 00:04:51 +0300

    show details on second screen

--- src/MovieInfoScreen.tsx
+++ src/MovieInfoScreen.tsx
@@ -1,7 +1,7 @@
 
 import React, {FC} from "react";
 import {Movie} from "./components/MovieCard";
-import {Image, Text, SafeAreaView, StyleSheet} from "react-native";
+import {Image, Text, SafeAreaView, StyleSheet, ScrollView, View} from "react-native";
 import commonStyles from "./utils/styles";
 import {useSelector} from "react-redux";
 import {AppState} from "../App";
@@ -11,28 +11,51 @@ const MovieInfoScreen: FC = () => {
     const { selectedMovieID, movies } = useSelector((state: AppState) => state.moviesReducer)
     const selectedMovie = findMovieByID(movies, selectedMovieID);
 
+    function renderText(text: string): React.ReactElement {
+        return <Text style={styles.text}>{text}</Text>
+    }
+
+    function renderMainDetailsView(): React.ReactElement {
+        return (
+            <View style={styles.mainDetailsView}>
+                {renderText(`${selectedMovie?.rating}/10`)}
+                {renderText(`${selectedMovie?.title}`)}
+                {renderText(`${selectedMovie?.runtime}`)}
+            </View>
+        )
+    }
     return (
-        <SafeAreaView>
-            <Text>{selectedMovie?.title}</Text>
+        <SafeAreaView style={styles.container}>
+            <ScrollView contentContainerStyle={styles.contentView} style={styles.container}>
+                <Image style={styles.image} source={{uri: selectedMovie?.largeimage }} />
+                {renderMainDetailsView()}
+                <Text>{selectedMovie?.synopsis}</Text>
+            </ScrollView>
         </SafeAreaView>
     )
 };
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        alignItems: "center",
-        borderRadius: 10,
-        padding: 10,
-        ...commonStyles.containerWithShadow
+        flex: 1
+    },
+    contentView: {
+        alignItems: 'center'
     },
     image: {
-        height: 150,
-        width: 100
+        height: 500,
+        width: "100%"
     },
-    title: {
-        fontSize: 30
+    mainDetailsView: {
+        flexDirection: "row",
+        width: '100%',
+        justifyContent: "space-between",
+        paddingHorizontal: 10
+    },
+    text: {
+        fontSize: 15
     }
+
 });
 
 export default MovieInfoScreen
