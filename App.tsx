428272f2a800ec82aae98f11fd1c5b4e3074b535
Or Menashe <menesh2@gmail.com>
2021-03-30 09:24:55 +0300

    add redux tollkit to the project

--- App.tsx
+++ App.tsx
@@ -1,13 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {configureStore, createSlice} from "@reduxjs/toolkit";
+import {Provider} from "react-redux";
+
+const store = configureStore({
+  reducer: {},
+});
 
 export default function App(): React.ReactElement {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Provider store={store}>
+        <View style={styles.container}>
+          <Text>Open up App.tsx to start working on your app!</Text>
+          <StatusBar style="auto" />
+        </View>
+      </Provider>
   );
 }
 
